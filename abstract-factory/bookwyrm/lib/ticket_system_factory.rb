1c8fad1353dc92145e915226eb63ed686093b05b
Matt Vanderpol <matt@vanderpol.net>
2014-10-05 13:16:06 -0700

    Finished tests and classes.

--- abstract-factory/bookwyrm/lib/ticket_system_factory.rb
+++ abstract-factory/bookwyrm/lib/ticket_system_factory.rb
@@ -5,6 +5,13 @@ class TicketSystemFactory
 end
 
 class AbstractTicket
+  attr_accessor :title, :description
+
+  def initialize(title, description)
+    self.title = title
+    self.description = description
+  end
+
   def submit
     raise NotImplementedError, "#{self.class.name} does not implement submit()"
   end
@@ -12,7 +19,13 @@ end
 
 class JiraTicketFactory < TicketSystemFactory
   def create_ticket(title, description)
-    return JiraTicket.new(title, description)
+    return JiraTicket.new title, description
+  end
+end
+
+class JiraTicket < AbstractTicket
+  def submit
+    'Submitting Jira ticket'
   end
 end
 
@@ -22,9 +35,33 @@ class GitHubTicketFactory < TicketSystemFactory
   end
 end
 
+class GitHubIssue < AbstractTicket
+  def submit
+    'Submitting GitHub issue'
+  end
+end
+
 class PivotalTicketFactory < TicketSystemFactory
   def create_ticket(title, description)
     return PivotalTicket.new(title, description)
+  end
+end
+
+class PivotalTicket < AbstractTicket
+  def submit
+    'Submitting a PivotalTracker ticket'
+  end
+end
+
+class Application
+  attr_accessor :ticket_system
+
+  def initialize(ticket_system)
+    self.ticket_system = ticket_system
+  end
+
+  def create_ticket(title, description)
+    ticket_system.create_ticket(title, description)
   end
 end
 
--- abstract-factory/bookwyrm/spec/ticketing_system_factory_spec.rb
+++ abstract-factory/bookwyrm/spec/ticketing_system_factory_spec.rb
@@ -10,10 +10,64 @@ describe TicketSystemFactory do
 end
 
 describe AbstractTicket do
- it "#send raises error" do
-   ticket = AbstractTicket.new
+ it "#submit raises error" do
+   ticket = AbstractTicket.new('title', 'description')
    expect {
      ticket.submit
    }.to raise_error(NotImplementedError, /does not implement submit/)
  end
+end
+
+describe JiraTicketFactory do
+  it "creates Jira tickets" do
+    factory =  JiraTicketFactory.new
+    ticket = factory.create_ticket('title', 'description')
+    expect(ticket.class.name).to eq('JiraTicket')
+  end
+end
+
+
+describe Application do
+  let(:title) { 'Ticket title' }
+  let(:description) { 'Ticket description' }
+
+  context "with a JiraTicketFactory" do
+    let(:application) { Application.new(JiraTicketFactory.new) }
+
+    it "creates Jira tickets" do
+      expect(application.create_ticket(title, description).class.name).to eq('JiraTicket')
+    end
+
+    it "creates tickets that can submit" do
+      ticket = application.create_ticket(title, description)
+      expect(ticket.submit).to eq('Submitting Jira ticket')
+    end
+  end
+
+  context "with a GitHubTicketFactory" do
+    let(:application) { Application.new(GitHubTicketFactory.new) }
+
+    it "creates GitHub issues" do
+      expect(application.create_ticket(title, description).class.name).to eq('GitHubIssue')
+    end
+
+    it "creates tickets that can submit" do
+      ticket = application.create_ticket(title, description)
+      expect(ticket.submit).to eq('Submitting GitHub issue')
+    end
+  end
+
+  context "with a PivotalTicketFactory" do
+    let(:application) { Application.new(PivotalTicketFactory.new) }
+
+    it "creates Pivotal tickets" do
+      expect(application.create_ticket(title, description).class.name).to eq('PivotalTicket')
+    end
+
+    it "creates tickets that can submit" do
+      ticket = application.create_ticket(title, description)
+      expect(ticket.submit).to eq('Submitting a PivotalTracker ticket')
+    end
+
+  end
 end
